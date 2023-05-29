33585a77c60bb39d6e3b16dfc43e0324813ae7cc
Lanmiemie <fatdeadpanda@163.com>
2023-05-29 22:38:51 +0800

    Qrcode Login

--- testapi.py
+++ testapi.py
@@ -37,64 +37,68 @@ def get_login_qrcode():
 
     print(relativeUrl)
 
-    get_scan_result(Encodesceneid)
+    def get_scan_result(EncodeSceneId):
 
-    return Encodesceneid
+        url = 'https://appapi.xueanquan.com/usercenter/api/v5/wx/scan-result?encodeSceneId=' + EncodeSceneId
 
+        headers = {'Accept': '*//*',
+                'Accept-Encoding': 'gzip',
+                'Accept-Language': 'zh-CN,zh;q=0.9',
+                'Connection': 'keep-alive',
+                'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Host': 'appapi.xueanquan.com',
+                'Origin': 'https://login.xueanquan.com',
+                'Referer': 'https://login.xueanquan.com/login?type=codeLogin',
+                #'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
+                'sec-ch-ua-mobile': '?0',
+                'sec-ch-ua-platform': 'Windows',
+                'Sec-Fetch-Dest': 'empty',
+                'Sec-Fetch-Mode': 'cors',
+                'Sec-Fetch-Site': 'same-origin',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
+                'X-Requested-With': 'XMLHttpRequest'
+                }
+        
+        data = {' '}
 
-def get_scan_result(EncodeSceneId):
+        def checkstatus():
+            res = requests.post(url=url, headers=headers, data=data)
+            #print(res.text)
+            global timer
+            timer = threading.Timer(0.2,checkstatus)
+            # 获取登陆状态
+            getstatuscode = re.findall('"status":"(.*?)",', res.text)
+            statuscode = str(getstatuscode).replace("'",'').replace(']','').replace('[','')
+            # 判断登陆状态
+            if statuscode == 'Error':
+                print('扫码已过期，正在重新获取')
+                # 重新调用获取函数
+                get_login_qrcode()
+                return 0
+            elif  statuscode == 'Success':
+                # 当状态码为登陆成功时获取用户信息
 
-    url = 'https://appapi.xueanquan.com/usercenter/api/v5/wx/scan-result?encodeSceneId=' + EncodeSceneId
+                useridcookie = re.findall('UserID=(.*?);', str(res.headers['Set-Cookie']))
+                userid = str(re.findall('"userId":"(.*?)",', res.text)).replace("'",'').replace(']','').replace('[','')
+                username = str(re.findall('"userName":"(.*?)",', res.text)).replace("'",'').replace(']','').replace('[','')
+                name = str(re.findall('"trueName":"(.*?)",', res.text)).replace("'",'').replace(']','').replace('[','')
+                print("扫码成功\n"+"姓名: " + name + ", 用户ID : " + userid + ", 用户名: " + username +"\n正在获取cookie")
+                print(useridcookie)
 
-    headers = {'Accept': '*//*',
-               'Accept-Encoding': 'gzip',
-               'Accept-Language': 'zh-CN,zh;q=0.9',
-               'Connection': 'keep-alive',
-               'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-               'Host': 'appapi.xueanquan.com',
-               'Origin': 'https://login.xueanquan.com',
-               'Referer': 'https://login.xueanquan.com/login?type=codeLogin',
-               #'sec-ch-ua': '"Not A;Brand";v="99", "Chromium";v="100", "Google Chrome";v="100"',
-               'sec-ch-ua-mobile': '?0',
-               'sec-ch-ua-platform': 'Windows',
-               'Sec-Fetch-Dest': 'empty',
-               'Sec-Fetch-Mode': 'cors',
-               'Sec-Fetch-Site': 'same-origin',
-               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-               'X-Requested-With': 'XMLHttpRequest'
-               }
-    
-    data = {' '}
+                # 取消循环
+                timer.cancel()
+                return 0
+            elif  statuscode == 'Wait':
+                print("等待扫码")
+            else:
+                print('未知状态\n'+ statuscode)
 
-    def checkstatus():
-        res = requests.post(url=url, headers=headers, data=data)
-        #print(res.text)
-        global timer
-        timer = threading.Timer(0.5,checkstatus)
-        getstatuscode = re.findall('"status":"(.*?)",', res.text)
-        statuscode = str(getstatuscode).replace("'",'').replace(']','').replace('[','')
-        if statuscode == 'Error':
-            print('扫码已过期，正在重新获取')
-            encodeid = get_login_qrcode()
-            return 0
-        elif  statuscode == 'Success':
-            useridcookie = re.findall('UserID=(.*?);', str(res.headers['Set-Cookie']))
-            userid = re.findall('"userId":="(.*?)",', res.text)
-            username = re.findall('"userName":="(.*?)",', res.text)
-            name = re.findall('"trueName":="(.*?)",', res.text)
-            print("扫码成功\n"+"姓名: " + str(name) + ", 用户ID : " + str(userid) + ", 用户名: " + str(username) +"\n正在获取cookie")
-            print(useridcookie)
-            timer.cancel()
-            return 0
-        elif  statuscode == 'Wait':
-            print("等待扫码")
-        else:
-            print('未知状态\n'+ statuscode)
+            timer = threading.Timer(0.2,checkstatus)
+            timer.start()
 
         timer = threading.Timer(0.2,checkstatus)
         timer.start()
 
-    timer = threading.Timer(0.2,checkstatus)
-    timer.start()
+    get_scan_result(Encodesceneid)
 
-encodeid = get_login_qrcode()
+get_login_qrcode()
